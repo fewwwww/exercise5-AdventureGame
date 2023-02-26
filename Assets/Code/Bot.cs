7ac0a46e7ef0d1e67f370205fc50a1ae49e644cb
Suning Yao <sy2940@nyu.edu>
2023-02-25 23:09:01 -0500

    Init with contents in class

---
+++ Assets/Code/Bot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    NavMeshAgent _navMeshAgent;
+    GameObject player;
+
+    void Start()
+    {
+        // Get the NavMeshAgent component
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        // Get the player
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        // Start chasing the player
+        StartCoroutine(ChasePlayer());
+    }
+
+    // Chase the player
+    IEnumerator ChasePlayer()
+    {
+        while (true)
+        {
+            // Wait a second
+            yield return new WaitForSeconds(1f);
+            // Set the destination to the player's position
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+}
---
+++ Assets/Code/Start.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    public string sceneName;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
